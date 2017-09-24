c7f5afe7e9365ee9b8337f8ef43dbbf18e1f4d70
St4lker <alexandrescotte@laposte.net>
2017-09-24 23:35:12 +0200

    - Configuration MVVM

---
+++ GoMuscuApp/GoMuscuApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using GoMuscuApp.Models.MVVM;
+using GoMuscuApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GoMuscuApp.ViewModels
+{
+    public class MainPageViewModel : CustomNotifyPropertyChanged
+    {
+        private Command _loadPage1;
+
+        public Command LoadPage1
+        {
+            get { return _loadPage1; }
+            set { _loadPage1 = value; }
+        }
+
+        public MainPageViewModel()
+        {
+            this.LoadPage1 = new Command(async () => await LoadPage());
+        }
+
+        private async Task LoadPage()
+        {
+            var page = new SessionDetailsPage();
+            await Application.Current.MainPage.Navigation.PushAsync(page);
+            NavigationPage.SetHasBackButton(page, true);
+            //NavigationPage.SetHasNavigationBar(page, false);
+            //NavigationPage.SetBackButtonTitle(page, "Back");
+        }
+    }
+}
---
+++ GoMuscuApp/GoMuscuApp/ViewModels/SessionDetailsPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using GoMuscuApp.Models.MVVM;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoMuscuApp.ViewModels
+{
+    public class SessionDetailsPageViewModel : CustomNotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int _myVar;
+
+        public int MyProperty
+        {
+            get
+            {
+                return _myVar;
+            }
+            set
+            {
+                _myVar = value;
+                this.OnPropertyChanged("MyProperty");
+            }
+        }
+
+    }
+}
