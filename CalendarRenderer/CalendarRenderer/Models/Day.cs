94f884933d9e54590891a912cdfee6b705a3e827
St4lker <alexandrescotte@laposte.net>
2017-10-05 23:59:23 +0200

    - Add calendar control project

---
+++ CalendarRenderer/CalendarRenderer/Models/Day.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarRenderer.Models
+{
+    public class Day
+    {
+        public int numberDay { get; set; }
+        public string nameDay { get; set; }
+    }
+}
---
+++ CalendarRenderer/CalendarRenderer/Models/Week.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarRenderer.Models
+{
+    public class Week
+    {
+        public ObservableCollection<Day> Days { get; set; }
+
+        public Week()
+        {
+            Days = new ObservableCollection<Day>();
+        }
+    }
+}
